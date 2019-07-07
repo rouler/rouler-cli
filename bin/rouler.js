a87e592e596479f752e4ca1db39564a6930bd5e7
xph <455835415@qq.com>
2019-07-07 20:08:41 +0800

    command tool init

---
+++ bin/rouler.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const commander = require('commander');
+const program = new commander.Command();
+
+program.version('0.0.1', '-v, --version')
+    .usage('<command> [project name]')
+    .option('-i, --init <project name>', 'init project')
+    .option('-r, --remove <module name>', 'module name')
+    .on('--help', () => {
+        // todo: exsamples for --help
+    })
+    .parse(process.argv);
+
+// init command
+if (program.init) {
+    let name = program.init;
+    createRoot(name);
+}
+
+// remove command
+if (program.remove) {
+    // todo:
+}
+
+function createRoot(target) {
+    if (glob.sync('*').filter(name => {
+        const entity = path.resolve(process.cwd(), path.join('.', name));
+        return name === target && fs.statSync(entity).isDirectory();
+    }).length > 0) {
+        console.log(`project ${target} exists`);
+        return;
+    }
+    createDir(path.basename(process.cwd()) === target ? '.' : target);
+}
+
+//create dir
+function createDir(root) {
+    if (root !== '.') {
+        fs.mkdirSync(root);
+    } else {
+        // todo: current dir
+    }
+}
